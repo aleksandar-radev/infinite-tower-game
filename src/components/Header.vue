63546d7e1c3cdee82871ee9292f1f9b944b620ae
Aleksandar Radev <alexander.radev.96@gmail.com>
2021-08-28 15:21:06 +0300

    add header + css

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template lang="">
+<div>
+  <ul class="grid grid-cols-5">
+    <li>Gold</li>
+    <li>crystals</li>
+    <li>level</li>
+    <li>soul lvl</li>
+    <li>rebirth stones</li>
+  </ul>
+</div>
+</template>
+
+<script>
+//  import MenuItem from './GameHeaderMenuItem.vue'
+
+export default {
+  name: 'Header',
+  components: {},
+  props: {},
+  methods: {},
+  data () {
+    return {}
+  },
+  created () {
+    return {}
+  }
+}
+</script>
+
+<style module lang="scss">
+
+</style>
